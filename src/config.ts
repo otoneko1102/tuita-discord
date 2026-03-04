92323524e59b4930843584b123f64893bb96b845
otoneko1102 <pineapple2006.11.2@gmail.com>
2026-03-04 06:20:09 +0100

    名称/説明追加

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,8 @@ export interface Config {
   ownerId?: string;
   tag: string;
   warningMs: number;
+  name: string;
+  description: string;
 }
 
 const config: Config = {
@@ -12,6 +14,8 @@ const config: Config = {
   ownerId: process.env.OWNER,
   tag: "[[tuita]]",
   warningMs: 3000,
+  name: "対多",
+  description: "唯一偽中国語使用可能掲示板\n🌱 草。非常愉快、爆笑投稿用\n👍 親指。良仕事、高評価投稿用\n🍬 飴。可哀想、同情、及元気譲渡用"
 };
 
 export default config;
